4cad3324cd2018d141da9897b3f770f919a3d7bf
Blackie01 <onioludamola@gmail.com>
2023-08-13 23:51:38 +0100

    getting started

---
+++ src/app/GlobalComponents/Navigation.jsx
@@ -0,0 +1,17 @@
+export default function NavBar () {
+    return (
+        <section>
+            <div>
+
+            </div>
+
+            <div className="flex bg-[#1F2321}">
+                <span>Home</span>
+                <span>About Us</span>
+                <span>Cars</span>
+                <span>Services</span>
+                <span>Contacts</span>
+            </div>
+        </section>
+    )
+}
