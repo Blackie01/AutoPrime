6067de1d63339c80f604b21efe41ba9a413081aa
Blackie01 <onioludamola@gmail.com>
2023-10-16 23:43:03 +0100

    oun

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LandingPage () {
+    return (
+        <section>
+            <div id='hero'>
+            <img style="width: 100%; height: 100%" src="https://via.placeholder.com/1645x924" />
+            </div>
+        </section>
+    )
+}
